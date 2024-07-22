7cdae79bd415d45686b6060849580f860f10352f
uzair02 <chemsa02@gmail.com>
2024-07-22 12:38:45 +0500

    Backend removed

--- backend/app/create_db.py
+++
@@ -1,5 +0,0 @@
-"""Module to create database tables."""
-
-from utils.database import Base, engine
-
-Base.metadata.create_all(bind=engine)
--- backend/app/utils/logging_config.py
+++
@@ -1,3 +0,0 @@
-from loguru import logger
-
-logger.add("logs/app.log", rotation="500 MB", retention="10 days", backtrace=True, diagnose=True)
